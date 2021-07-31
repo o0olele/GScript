d9b1696ad59a82b714aa7a1f2fbcc06d01f0617c
o0olele <43069191+o0olele@users.noreply.github.com>
2021-07-31 20:25:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from common import *
 import cv2
 
 if __name__ == '__main__':
+    # init window mgr
+    # features include capture image and send click event
     win = WindowMgr('BlueStacks')
-    temps = TemplMgr('./img')
+    # init template mgr
+    # templates are used to detect the click area
+    temps = TemplMgr('./img/AzurLane')
 
     while True:
+        # detect every 1 sec
         time.sleep(1)
-
+        # cap image
         img = win.cap()
-
+        # detect the aim
         res = temps.detect(img)
-
+        # if have aims
         if len(res) > 0:
             print(res)
-
+        # click scheduler
         for item in res:
             if item[0] == Btn_Attack_Again:
+                # click
                 win.click(item[1][0] + 10, item[1][1] + 10)
