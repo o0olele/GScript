99e92308ce22b05de9fd9690f01498195d1bb86f
o0olele <43069191+o0olele@users.noreply.github.com>
2021-08-24 22:03:09 +0800

    [core] update control

--- core/control.py
+++ core/control.py
@@ -1,6 +1,17 @@
+from .window import Window, WinList
+
 
 class Controller:
     def __init__(self):
         """
         top controller
         """
+        self.win = None
+
+    def win_list(self):
+        return WinList()
+
+    def win_init(self, name: str):
+        self.win = Window(name)
+
+gcore = Controller()
