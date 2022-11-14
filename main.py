873c8dd98dd14e3c0ebff7cf8cd326382b5ae539
o0olele <43069191+o0olele@users.noreply.github.com>
2022-11-15 00:08:45 +0800

    [main] add btn

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import time
 import cv2
 
 if __name__ == '__main__':
+    # print(GetWindows())
     # init window mgr
     # features include capture image and send click event
-    win = WindowMgr('BlueStacks')
+    win = WindowMgr('雷电模拟器')
     # init template mgr
     # templates are used to detect the click area
     temps = TemplMgr('./img/AzurLane')
@@ -25,3 +26,12 @@ if __name__ == '__main__':
             if item[0] == Btn_Attack_Again:
                 # click
                 win.click(item[1][0] + 10, item[1][1] + 10)
+            elif item[0] == Btn_All_Retire:
+                win.click(item[1][0] + 10, item[1][1] + 10)
+                time.sleep(0.5)
+                win.click(item[1][0] + 400, item[1][1] + 10)
+            elif item[0] == Btn_Run:
+                win.click(item[1][0] + 70, item[1][1] - 150)
+            else:
+                win.click(item[1][0] + 10, item[1][1] + 10)
+
